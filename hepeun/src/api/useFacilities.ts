b05640d94f0900e39b82004746033f15000be184
dpqlsn <a01037111209@gmail.com>
2025-06-30 23:04:47 +0900

    feat (#6) :: 유지보수 용이

---
+++ hepeun/src/api/useFacilities.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import type { AxiosResponse } from "axios";
+import type { Facility, ApiResponse } from "../types/type";
+
+export default function useFacilities() {
+    const [facilities, setFacilities] = useState<Facility[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchFacilities = async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const res: AxiosResponse<ApiResponse> = await axios.get(
+            "https://api.odcloud.kr/api/15001020/v1/uddi:73a09ce6-7c10-4174-8be1-6cf139e3361e",
+            {
+                params: {
+                page: 1,
+                perPage: 100,
+                serviceKey: import.meta.env.VITE_API_KEY,
+                },
+            }
+            );
+
+            const data = res.data.data;
+
+            const filteredFacilities = data
+            .filter((item) => item.소재지도로명주소.includes("부산"))
+            .map((item, idx) => ({
+                id: `${item.시설명}-${idx}`,
+                name: item.시설명,
+                address: item.소재지도로명주소 || item.소재지지번주소 || "주소 없음",
+                phone: item.전화번호 || "정보 없음",
+                type: item.운영형태 || "정보 없음",
+                service: item.장애인편의시설정보 || "정보 없음",
+            }));
+
+            setFacilities(filteredFacilities);
+        } catch (error: unknown) {
+            setError("데이터를 불러오는 중 오류가 발생했습니다.");
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+        };
+
+        fetchFacilities();
+    }, []);
+
+    return { facilities, loading, error };
+}
--- hepeun/src/types/type.ts
+++ hepeun/src/types/type.ts
@@ -15,3 +15,21 @@ export interface facilities {
     facilities: Facility[];
     onSelect: (facility: Facility) => void;
 }
+
+export interface ApiResponse {
+    currentCount: number;
+    data: FacilityRaw[];
+    matchCount: number;
+    page: number;
+    perPage: number;
+    totalCount: number;
+}
+
+export interface FacilityRaw {
+    시설명: string;
+    소재지도로명주소: string;
+    소재지지번주소: string;
+    전화번호: string;
+    운영형태: string;
+    장애인편의시설정보: string;
+}
