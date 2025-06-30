ac3165e4af1cb81d37c5b16b4ba672ebfbbb28f1
dpqlsn <a01037111209@gmail.com>
2025-06-30 22:47:17 +0900

    fix (#2) :: 스타일링

--- hepeun/src/components/FacilityList.tsx
+++ hepeun/src/components/FacilityList.tsx
@@ -1,26 +1,35 @@
 import FavoriteButton from "./FavoriteButton";
 import type { facilities } from "../types/type";
-
+import styled from '@emotion/styled';
 
 export default function FacilityList({ facilities, onSelect }: facilities) {
     return (
-        <div>
-        {facilities.map((f) => (
-            <div
-            key={f.id}
-            style={{
-                border: "1px solid #ccc",
-                marginBottom: 8,
-                padding: 10,
-                cursor: "pointer",
-            }}
-            onClick={() => onSelect(f)}
-            >
-            <h3>{f.name}</h3>
-            <p>{f.address}</p>
-            <FavoriteButton facility={f} />
-            </div>
-        ))}
-        </div>
+        <Container>
+            {facilities.map((f) => (
+                <Key
+                key={f.id}
+                onClick={() => onSelect(f)}
+                >
+                <h3>{f.name}</h3>
+                <p>{f.address}</p>
+                <FavoriteButton facility={f} />
+                </Key>
+            ))}
+        </Container>
     );
 }
+
+const Container = styled.div`
+    width: 100%;
+`;
+
+const Key = styled.div`
+    border: 1px solid #ccc;
+    margin-bottom: 1rem;
+    padding: 10px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #f9f9f9;
+    }
+`;
