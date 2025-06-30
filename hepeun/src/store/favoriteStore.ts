d5f349585dbdea8a053d054bc939394e799ed0c5
dpqlsn <a01037111209@gmail.com>
2025-06-30 22:27:16 +0900

    feat (#1) :: 커스텀 훅 및 Zustand 상태관리 저장

---
+++ hepeun/src/store/favoriteStore.ts
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+
+interface Props {
+    id: string;
+    name: string;
+    address: string;
+    phone: string;
+    type: string;
+    service: string;
+}
+
+interface FavoriteState {
+    favorites: Props[];
+    toggleFavorite: (facility: Props) => void;
+    isFavorite: (id: string) => boolean;
+}
+
+export const useFavoriteStore = create<FavoriteState>((set, get) => ({
+    favorites: JSON.parse(localStorage.getItem("favorites") || "[]"),
+    toggleFavorite: (facility) => {
+        const favorites = get().favorites;
+        const exists = favorites.find((f) => f.id === facility.id);
+        const updated = exists
+        ? favorites.filter((f) => f.id !== facility.id)
+        : [...favorites, facility];
+        localStorage.setItem("favorites", JSON.stringify(updated));
+        set({ favorites: updated });
+    },
+    isFavorite: (id) => {
+        return get().favorites.some((f) => f.id === id);
+    },
+}));
